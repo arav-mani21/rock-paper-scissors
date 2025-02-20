69a1700253101d50c864cc82559c48033c56bc32
Aravind Manivannan <192905623+arav-mani21@users.noreply.github.com>
2025-02-19 21:26:07 -0500

    added getHumanChoice() function, which prompts users to enter input added playRound() function, which plays out a single rock paper scissors round added playGame() function, which plays out 5 rock paper scissors rounds and declares a winner

--- script.js
+++ script.js
@@ -1,5 +1,62 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice(){
     const values = ['rock','paper','scissors'];
     let num = Math.floor(Math.random() * values.length)
     return values[num];
+}
+
+function getHumanChoice(){
+    return prompt('rock, paper, scissors, shoot!');
+}
+
+function playRound(humanChoice, computerChoice){
+    if (humanChoice == computerChoice){
+        console.log('Same input. Tie game!');
+    }else if (humanChoice == 'rock'){
+        if (computerChoice == 'scissors'){
+            console.log('You win! Rock beats scissors!')
+            humanScore++;
+        }else{
+            console.log('You lose! Paper beats rock!')
+            computerScore++;
+        }
+    }else if (humanChoice == 'paper'){
+        if (computerChoice == 'rock'){
+            console.log('You win! Paper beats rock!')
+            humanScore++;
+        }else{
+            console.log('You lose! Scissor beats paper!')
+            computerScore++;
+        }
+    }else{
+        if (computerChoice == 'paper'){
+            console.log('You win! Scissors beats paper!')
+            humanScore++;
+        }else{
+            console.log('You lose! Rock beats scissors!')
+            computerScore++;
+        }
+    }
+}
+
+function playGame(){
+    humanScore = 0;
+    computerScore = 0;
+
+    for(let i = 0; i < 5; i++){
+        playRound(getHumanChoice(),getComputerChoice());
+    }
+
+    if(humanScore == computerScore){
+        console.log('Tie game! No one wins');
+    }else if(humanScore > computerScore){
+        console.log('Human wins!');
+    }else{
+        console.log('Computer wins!');
+    }
+
+    console.log('Human round wins: ' + humanScore);
+    console.log('Computer round wins: ' + computerScore);
 }
